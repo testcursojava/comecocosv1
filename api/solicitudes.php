7fae11a38f38ad9ade76bbdf5f01ab2b91439f07
testcursojava <31422115+testcursojava@users.noreply.github.com>
2018-03-05 10:27:34 +0100

    lunes 2

--- api/solicitudes.php
+++ api/solicitudes.php
@@ -2,7 +2,7 @@
     session_start();
     include("model/Partidas.php");
     $partidas = new Partidas();
-    if(isset($_SERVER["user"])&&isset($_SERVER["partida"])&&$partidas->isAdmin($_SESSION["user"],$_SESSION["partida"])){
+    if(isset($_SESSION["user"])&&isset($_SESSION["partida"])&&$partidas->isAdmin($_SESSION["user"],$_SESSION["partida"])){
         include("model/Solicitudes.php");
         $solicitudes = new Solicitudes();
         echo json_encode($solicitudes->listar($_SESSION["partida"]));
--- fragments/partida.php
+++ fragments/partida.php
@@ -1,8 +1,67 @@
 
     
     <center><div class="tablero">Tablero</div>
+    <table>
+        <thead>
+            <tr><th>Nick</th><th>Acción</th></tr>
+        </thead>
+        <tbody id="solicitudes"></tbody>
+    </table>
     <button id="cerrar"><a href="../index.php">CERRAR</a></button></center>
 	<script>
+        var solicitudesDOM = document.getElementById("solicitudes");
+        function pintarSolicitud(solicitud){
+            if(solicitud.user && solicitud.partida){
+                var fila = document.createElement("tr");
+                var columnaNick = document.createElement("td");
+                columnaNick.innerHTML = solicitud.user.nick;
+                fila.appendChild(columnaNick);
+                var columnaAccion = document.createElement("td");
+                var buttonAceptar = document.createElement("button");
+                var buttonDenegar = document.createElement("button");
+                buttonAceptar.innerHTML = "ACEPTAR";
+                buttonDenegar.innerHTML = "DENEGAR";
+                columnaAccion.appendChild(buttonAceptar);
+                columnaAccion.appendChild(buttonDenegar);
+                fila.appendChild(columnaAccion);
+                solicitudesDOM.appendChild(fila);
+                buttonAceptar.onclick = function(){
+                    $.ajax({
+                       type:"POST",
+                       url:"/api/aceptarSolicitud.php",
+                       data:{user:solicitud.user.id,
+                                partida:solicitud.partida.id},
+                        dataType: "json",
+                        success: function(r){}
+                    });
+                }
+                buttonDenegar.onclick = function(){
+                    $.ajax({
+                       type:"POST",
+                       url:"/api/denegarSolicitud.php",
+                       data:{user:solicitud.user.id,
+                                partida:solicitud.partida.id},
+                        dataType: "json",
+                        success: function(r){}
+                    });
+                }
+            }
+        }
+        function listarSolicitudes(){
+            $.ajax({
+                type: 'GET',
+                url: "/api/solicitudes.php",
+                dataType: "json",
+                success: function(r){
+                    solicitudesDOM.innerHTML = "";
+                    for(var i=0;i<r.length;i++)
+                        pintarSolicitud(r[i]);
+                }
+            });
+        }
+        setInterval(function(){
+            listarSolicitudes();
+        },1000);
 		document.getElementById("cerrar").onclick = function(){
 			
 		};
